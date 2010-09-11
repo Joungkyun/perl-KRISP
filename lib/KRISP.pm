494533a2fa8dfd6e27b094b2527ae5bceadee4d1
JoungKyun Kim <joungkyun@daum.net>
2010-09-11 09:05:50 +0000

    fixed license to LGPL

--- lib/KRISP.pm
+++ lib/KRISP.pm
@@ -20,7 +20,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
 
 our $VERSION = '2.1.0';
-my $revision = '$Id: KRISP.pm,v 1.4 2010-09-11 08:19:47 oops Exp $';
+my $revision = '$Id: KRISP.pm,v 1.5 2010-09-11 09:05:50 oops Exp $';
 
 require XSLoader;
 XSLoader::load('KRISP', $VERSION);
@@ -285,17 +285,17 @@ JoungKyun.Kim , E<lt>admin@oops.orgE<gt>
 Copyright (C) 2010 by JoungKyun.Kim
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published
+by the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU LESSER GENERAL PUBLIC LICENSE for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
+You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+along with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
