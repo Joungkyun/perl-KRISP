19130be36dc8946521221addd285e763ad122eff
JoungKyun Kim <joungkyun@daum.net>
2011-04-27 05:56:37 +0000

    2.1.1 tree start

--- version.h
+++ version.h
@@ -2,8 +2,8 @@
  * $Id: version.h,v 1.3 2010-09-11 08:19:47 oops Exp $
  */
 
-#define MODVER "2.1.0"
-#define MODUVER "002001000"
+#define MODVER "2.1.1"
+#define MODUVER "002001001"
 
 /*
  * Local variables:
