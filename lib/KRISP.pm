76dc2b7b1df12bc27021b1a0cee4c07bea6e29e8
joungkyun <joungkyun@gmail.com>
2016-07-14 00:16:26 +0900

    update copyright

--- lib/KRISP.pm
+++ lib/KRISP.pm
@@ -1,5 +1,20 @@
 package KRISP;
 
+# Copyright (C) 2016 by JoungKyun.Kim <http://oops.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published
+# by the Free Software Foundation; either version 2.1 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU LESSER GENERAL PUBLIC LICENSE for more details.
+#
+# You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # Local variables:
 # tab-width: 4
 # c-basic-offset: 4
@@ -328,11 +343,11 @@ JoungKyun.Kim , E<lt>admin@oops.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 by JoungKyun.Kim
+Copyright (C) 2016 by JoungKyun.Kim
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published
-by the Free Software Foundation; either version 2 of the License, or
+by the Free Software Foundation; either version 2.1 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
